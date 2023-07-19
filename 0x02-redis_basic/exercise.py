1312d2d5a7669421a35a3e43dac90811b30386a7
sagebeme <eudicas5k@gmail.com>
2023-07-19 23:48:36 +0300

    redis basics setting up a uuid in a cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+"""
+import random
+from typing import Union, Optional, Callable
+from uuid import uuid4
+
+import redis
+
+class Cache:
+    """
+    creating a cache to store something
+    """
+    def __init__(self):
+        """
+        constructor that calls the redis
+        the flash the instance
+        """
+        self._redis = redis.Redis
+        self._redis.flushdb
+
+    
+    def store(self, data : UnionOfTypes) -> str:
+        """
+        method that generates a random key
+        for the UUID
+        """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
