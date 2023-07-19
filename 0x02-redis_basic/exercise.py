e300c0e3cb097b337e1317b9c004c2d2dd9f9dae
sagebeme <eudicas5k@gmail.com>
2023-07-20 00:06:25 +0300

    gets the cache files stored in the redis database and assigns an id

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
 """
+import sys
 import random
 from typing import Union, Optional, Callable
 from uuid import uuid4
 
 import redis
 
+ManyTypes = Union[str, bytes, int, float]
 
 class Cache:
     """
@@ -23,7 +25,7 @@ class Cache:
 
     @count_calls
     @call_history
-    def store(self, data: UnionOfTypes) -> str:
+    def store(self, data: ManyTypes) -> str:
         """
         method that generates a random key
         for the UUID
@@ -31,3 +33,25 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> ManyTypes:
+        """
+        converts data to the desired
+        format
+        """
+        if fn :
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+    
+    def get_str(self: bytes) -> str:
+        """
+        gets the input user data str
+        """
+        return self.decode("utf-8")
+    
+    def get_int(self: bytes) -> int:
+        """
+        gets the input int ID
+        """
+        return int.from_bytes(self, sys.byteorder)
